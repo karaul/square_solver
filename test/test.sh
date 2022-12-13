290618315b070f3c365b0417dcbc27e3f28fe605
karaul <karaul@gmail.com>
2022-12-13 22:47:41 +0200

    test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+PROG=${1:-"../build/square_solver"}
+TESTFILE=${2:-"test.txt"}
+
 while read line; do 
-    ARGS=$( echo $line | sed 's/^.*exe//1;'  )
-    echo 
-    echo $ARGS
-    ../build/square_solver $ARGS
-done < test.txt
+    ARGS=$( echo $line ) # | sed 's/^.*exe//1;'  )
+    echo  input:
+    echo $PROG $ARGS
+    echo output:
+    $PROG  $ARGS
+done <  ${TESTFILE}
 
